e8b7e55e695799d07d7a6c4f852f11113a9c20db
Mikem <Mikemangan00@gmail.com>
2017-03-07 10:53:20 -0500

    updated to use tablayout

--- app/src/main/java/com/example/android/miwok/CategoryAdapter.java
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -10,26 +10,49 @@ import android.support.v4.app.FragmentPagerAdapter;
 
 public class CategoryAdapter extends FragmentPagerAdapter {
 
-    public CategoryAdapter(FragmentManager fm) {
+    //integer to count number of tabs
+    int tabCount;
+
+    private String tabTitles[] = new String[] { "Numbers", "Family", "Colors", "Phrases" };
+
+    public CategoryAdapter(FragmentManager fm, int tabCount) {
         super(fm);
+        //Initializing tab count
+        this.tabCount= tabCount;
     }
 
+
+    //Overriding method getItem
     @Override
     public Fragment getItem(int position) {
-        if (position == 0) {
-            return new NumbersFragment();
-        } else if (position == 1){
-            return new FamilyFragment();
-        } else if (position == 2) {
-            return new ColorsFragment();
-        } else {
-            return new PhrasesFragment();
+        //Returning the current tabs
+        switch (position) {
+            case 0:
+                NumbersFragment numbersFragment = new NumbersFragment();
+                return numbersFragment;
+            case 1:
+                FamilyFragment familyFragment = new FamilyFragment();
+                return familyFragment;
+            case 2:
+                ColorsFragment colorsFragment = new ColorsFragment();
+                return colorsFragment;
+            case 3:
+                PhrasesFragment phrasesFragment = new PhrasesFragment();
+                return phrasesFragment;
+            default:
+                return null;
         }
     }
 
+    //Overriden method getCount to get the number of tabs
     @Override
     public int getCount() {
-        return 4;
+        return tabCount;
     }
 
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return tabTitles[position];
+    }
 }
--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -17,13 +17,21 @@ package com.example.android.miwok;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //This is our tablayout
+    private TabLayout tabLayout;
+
+    //This is our viewPager
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +40,30 @@ public class MainActivity extends AppCompatActivity {
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
 
+        //Adding toolbar to the activity
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //Initializing the tablayout
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+
+        //Adding the tabs using addTab() method
+        tabLayout.addTab(tabLayout.newTab());
+        tabLayout.addTab(tabLayout.newTab());
+        tabLayout.addTab(tabLayout.newTab());
+        tabLayout.addTab(tabLayout.newTab());
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
         // Find the view pager that will allow the user to swipe between fragments
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
 
         // Create an adapter that knows which fragment should be shown on each page
-        CategoryAdapter adapter = new CategoryAdapter(getSupportFragmentManager());
+        CategoryAdapter adapter = new CategoryAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
 
         // Set the adapter onto the view pager
         viewPager.setAdapter(adapter);
+
+        tabLayout.setupWithViewPager(viewPager);
 
     }
 }
